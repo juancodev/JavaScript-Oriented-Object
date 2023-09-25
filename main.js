a5d04ca4ee4875be0a75c786d2495295220f9bff
juancodev <montillasanchezjuancarlos@gmail.com>
2023-09-24 22:26:24 -0400

    [⚡ADD] Class: syntaxic sugar

--- main.js
+++ main.js
@@ -35,3 +35,50 @@ Student.prototype.approvedCourse = function (newCourse) {
 
 const student1 = new Student("Juan", 24, "Software Engineer");
 const student2 = new Student("Cynthia", 24, "Marketer");
+
+// Prototype Class
+
+class StudentOfCourse {
+  constructor(name, age, career) {
+    this.name = name;
+    this.age = age;
+    this.career = career;
+    this.course = [];
+  }
+
+  approvedCourse(newCourse) {
+    this.course.push(newCourse);
+  }
+}
+
+const juan = new StudentOfCourse("Juan", 24, "Software Engineer");
+const cynthia = new StudentOfCourse("Cynthia", 24, "Marketer");
+
+// class with one params
+
+class StudentOfCourseOneParams {
+  constructor({
+    name,
+    age,
+    career,
+    email,
+    course = [],
+  }) {
+    this.name = name;
+    this.age = age;
+    this.career = career;
+    this.email = email;
+    this.course = course;
+  }
+
+  approvedCourse(newCourse) {
+    this.course.push(newCourse);
+  }
+}
+
+const juanOneParams = new StudentOfCourseOneParams({
+  name: "Juan",
+  age: 24,
+  career: "Software Engineer",
+  email: "montillasanchezjuancarlos@gmail.com"
+})
